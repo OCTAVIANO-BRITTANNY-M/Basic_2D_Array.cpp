5cee3588bdf4f3ef9781ca875a4ddcfc892e5b67
Open <425001059@ntc.edu.ph>
2026-09-14 02:35:27 +0800

    PART B: Matrix Traversal - Instructor Example

--- main.cpp
+++ main.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int matrix[2][5] = {
-        {1, 2, 3, 4, 5},
-        {6, 7, 8, 9, 10}
-    };
-
-    cout << "matrix[1][3] = " << matrix[1][3] << endl;
-
-    int totalElements = 0;
-    int sum = 0;
-
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 5; j++) {
-            cout << matrix[i][j] << "\t";
-            totalElements++;
-            sum += matrix[i][j];
+void displayMatrix(int mat[][4], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << mat[i][j] << "\t";
         }
-
         cout << endl;
     }
+}
 
-    cout << "Total elements: " << totalElements << endl;
-    cout << "Sum of all elements: " << sum << endl;
+int main() {
+    int matrix[2][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8}
+    };
+
+    displayMatrix(matrix, 2, 4);
 
     return 0;
 }
