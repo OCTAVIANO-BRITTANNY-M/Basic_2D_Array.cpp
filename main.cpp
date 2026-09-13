63f8676da3199c969a42df9aefdaba6ac0bf37e3
Open <425001059@ntc.edu.ph>
2026-09-14 02:42:00 +0800

    PART B: Matrix Tranversal - Student Task

--- main.cpp
+++ main.cpp
@@ -11,12 +11,13 @@ void displayMatrix(int mat[][4], int rows, int cols) {
 }
 
 int main() {
-    int matrix[2][4] = {
+    int matrix[3][4] = {
         {1, 2, 3, 4},
-        {5, 6, 7, 8}
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
     };
 
-    displayMatrix(matrix, 2, 4);
+    displayMatrix(matrix, 3, 4);
 
     return 0;
 }
